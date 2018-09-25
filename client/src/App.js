12bf15e2305c676e290e2e1fad8153b6e8bf0eee
Muscle Everest <danza_seppia@yahoo.com>
2018-09-24 21:47:55 -0500

    server.js fixes

--- client/src/App.js
+++ client/src/App.js
@@ -12,57 +12,62 @@ class App extends Component {
     topic: "",
     startYear: "",
     endYear: ""
-};
+  };
 
-componentDidMount() {
+  componentDidMount() {
     this.loadArticles();
-}
+  }
 
-loadArticles = () => {
+  loadArticles = () => {
     API.getArticles()
-        .then(res =>
-            this.setState({ articles: res.data, topic: "", startYear: "", endYear: "" })
-        )
-        .catch(err => console.log(err));
-};
+      .then(res =>
+        this.setState({ articles: res.data, topic: "", startYear: "", endYear: "" })
+      )
+      .catch(err => console.log(err));
+  };
 
-deleteArticle = id => {
+  deleteArticle = id => {
     API.deleteArticle(id)
-        .then(res => this.loadArticles())
-        .catch(err => console.log(err));
-};
+      .then(res => this.loadArticles())
+      .catch(err => console.log(err));
+  };
 
-handleInputChange = event => {
+  handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
-        [name]: value
+      [name]: value
     });
-};
+  };
 
-handleFormSubmit = event => {
+  handleFormSubmit = event => {
     event.preventDefault();
     if (this.state.topic) {
-        API.saveArticle({
-            topic: this.state.topic,
-            startYear: this.state.startYear,
-            endYear: this.state.endYear
-        })
-            .then(res => this.loadArticles())
-            .catch(err => console.log(err));
+      API.saveArticle({
+        topic: this.state.topic,
+        startYear: this.state.startYear,
+        endYear: this.state.endYear
+      })
+        .then(res => this.loadArticles())
+        .catch(err => console.log(err));
     }
-};
+  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">New York Times Articles</h1>
         </header>
         <div class="container">
-          <Search />
-          <Results />
-          <Saved />
+          <div class="card">
+            <Search />
+          </div>
+          <div class="card">
+            <Results />
+          </div>
+          <div class="card">
+            <Saved />
+          </div>
         </div>
       </div>
     );
--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,4 +1,4 @@
-const db = require("../models");
+const db = require("../models/article");
 
 // Defining methods for the articlesController
 module.exports = {
